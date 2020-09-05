506addaf0f77778604c25fb8400de88f6a7c755e
truman-show <ljh86091@gmail.com>
2020-09-05 13:36:33 +0900

    로컬 클래스를 설명하라

--- this-is-java/src/main/java/chapter09/sec02_exam01_nestedclass_object/A.java
+++ this-is-java/src/main/java/chapter09/sec02_exam01_nestedclass_object/A.java
@@ -38,6 +38,26 @@ public class A {
     }
 
   }
+
+
+  void method() {
+    //로컬 클래스
+    class D {
+      int field1;
+//      static int field2;  static 멤버 필드는 추가할 수 없다
+
+      public D() {
+        System.out.println("D 객체가 생성됨.");
+      }
+
+      void method1() {
+      }
+//      static void method2(){} static 멤버 메서드는 추가할수 없다
+    }
+    D d = new D();
+    d.field1 = 3;
+  }
+
 }
 
 class Main {
@@ -56,5 +76,10 @@ class Main {
 
     A.C.field2 = 3;
     A.C.method2();
+
+    // 로컬클래스 (D) 를 사용할 방법은 없다.
+    // A 객체의 method() 가 호출될때 D 로컬클래스가 사용될것이다.
+    a.method();
+
   }
 }
