745bb35d4d3c21dd36d8a4762519868d6c6495c0
truman-show <ljh86091@gmail.com>
2020-10-24 12:03:04 +0900

    초난감 DAO를 이용하는 main메소드를 추가하라

--- toby-study/src/main/java/springbook/user/dao/UserDao.java
+++ toby-study/src/main/java/springbook/user/dao/UserDao.java
@@ -41,8 +41,8 @@ public class UserDao {
 
     User user = new User();
     user.setId(resultSet.getString("id"));
-    user.setId(resultSet.getString("name"));
-    user.setId(resultSet.getString("password"));
+    user.setName(resultSet.getString("name"));
+    user.setPassword(resultSet.getString("password"));
 
     resultSet.close();
     preparedStatement.close();
@@ -51,5 +51,23 @@ public class UserDao {
     return user;
   }
 
+  //테스트용 main()메소드
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    UserDao dao = new UserDao();
 
+    User user = new User();
+    user.setId("truman-show2");
+    user.setName("이지훈");
+    user.setPassword("1234");
+
+    dao.add(user);
+
+    System.out.println(user.getId() + " 등록 성공");
+
+    User user2 = dao.get(user.getId());
+    System.out.println(user2.getName());
+    System.out.println(user2.getPassword());
+    System.out.println(user2.getId() + "조회 성공");
+
+  }
 }
