56dfbe2c43914260288e0f7369dfaf2f988418b1
truman-show <ljh86091@gmail.com>
2020-10-25 19:19:07 +0900

    스프링(빈팩토리)가 사용할 설정정보 오브젝트를 추가하라

--- toby-study/src/main/java/springbook/user/dao/DaoFactory.java
+++ toby-study/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,9 +1,18 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
+
+  @Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시
   public UserDao userDao() {
-    NConnectionMaker connectionMaker = new NConnectionMaker();
-    UserDao userDao = new UserDao(connectionMaker);
-    return userDao;
+    return new UserDao(connectionMaker());
+  }
+
+  @Bean
+  public ConnectionMaker connectionMaker() {
+    return new DConnectionMaker();
   }
 }
