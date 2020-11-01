6b3197f09cc817b080817b18ff509f01788b58fa
truman-show <ljh86091@gmail.com>
2020-11-01 20:25:03 +0900

    JUnit 테스트로 전환하라

--- toby-study/src/main/java/springbook/user/dao/UserDao.java
+++ toby-study/src/main/java/springbook/user/dao/UserDao.java
@@ -44,7 +44,7 @@ public class UserDao {
 
     User user = new User();
     user.setId(resultSet.getString("id"));
-//    user.setName(resultSet.getString("name"));
+    user.setName(resultSet.getString("name"));
     user.setPassword(resultSet.getString("password"));
 
     resultSet.close();
@@ -54,37 +54,4 @@ public class UserDao {
     return user;
   }
 
-}
-
-class UserDaoTest {
-
-  //테스트용 main()메소드
-  public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-//    DaoFactory daoFactory = new DaoFactory();
-//    UserDao dao = daoFactory.userDao();
-
-    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-    UserDao dao = context.getBean("userDao", UserDao.class);
-
-    User user = new User();
-    user.setId("truman-show");
-    user.setName("이지훈");
-    user.setPassword("1234");
-
-    dao.add(user);
-
-    System.out.println(user.getId() + " 등록 성공");
-
-    User user2 = dao.get(user.getId());
-    if (!user.getName().equals(user2.getName())) {
-      System.out.println("테스트 실패 (name)");
-    } else if (!user.getPassword().equals(user2.getPassword())) {
-      System.out.println("테스트 실패 (password)");
-    }
-    else {
-      System.out.println("조회 테스트 성공");
-    }
-
-  }
 }
