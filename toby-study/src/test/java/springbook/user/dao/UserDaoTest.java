050dd067bfc36024790a319c537ebc8260cccf81
truman-show <ljh86091@gmail.com>
2020-11-01 22:21:06 +0900

    get() 테스트 기능을 보완하라
    - get()이 파라미터로 주어진 id에 해당하는 사용자를 가져온 것인지, 그냥 아무거나 가져온 것인지 검증하라

--- toby-study/src/test/java/springbook/user/dao/UserDaoTest.java
+++ toby-study/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -15,19 +15,23 @@ public class UserDaoTest {
   public void addAndGet() throws SQLException, ClassNotFoundException { // JUnit 테스트 메소드는 반드시 public 으로 선언돼야 한다.
     ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
     UserDao dao = context.getBean("userDao", UserDao.class);
+    User user1 = new User("hello0", "world0", "hello world0");
+    User user2 = new User("hello1", "world1", "hello world1");
 
     dao.deleteAll();
     assertThat(dao.getCount()).isSameAs(0);
 
-    User user = new User("truman-show", "이지훈", "1234");
+    dao.add(user1);
+    dao.add(user2);
+    assertThat(dao.getCount()).isSameAs(2);
 
-    dao.add(user);
-    assertThat(dao.getCount()).isSameAs(1);
+    User userget1 = dao.get(user1.getId());
+    assertThat(userget1.getName()).isEqualTo(user1.getName());
+    assertThat(userget1.getPassword()).isEqualTo(user1.getPassword());
 
-    User user2 = dao.get(user.getId());
-
-    assertThat(user2.getName()).isEqualTo(user.getName());
-    assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    User userget2 = dao.get(user2.getId());
+    assertThat(userget2.getName()).isEqualTo(user2.getName());
+    assertThat(userget2.getPassword()).isEqualTo(user2.getPassword());
   }
 
   @Test
