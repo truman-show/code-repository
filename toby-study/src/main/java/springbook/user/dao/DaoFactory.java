b8e40055b06e05f055a732a2d8476b95dea35896
truman-show <ljh86091@gmail.com>
2020-11-01 18:52:02 +0900

    자바코드설정방식을 이용하여 DataSource인터페이스를 적용하라 P 136

--- toby-study/src/main/java/springbook/user/dao/DaoFactory.java
+++ toby-study/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,18 +1,26 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration  // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
 
   @Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시
   public UserDao userDao() {
-    return new UserDao(connectionMaker());
+    return new UserDao(dataSource());
   }
 
   @Bean
-  public ConnectionMaker connectionMaker() {
-    return new DConnectionMaker();
+  public DataSource dataSource() {
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    dataSource.setDriverClass(org.h2.Driver.class);
+    dataSource.setUrl("jdbc:h2:tcp://localhost/~/springbook");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+    return dataSource;
   }
+
 }
