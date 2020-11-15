7794d5b747d96df8582501e9a9351fcbc8ea1331
truman-show <ljh86091@gmail.com>
2020-11-15 11:14:53 +0900

    카프카 예제 코드를 추가하라

---
+++ springboot-kafka-example/src/main/java/com/truman/show/config/KafkaMain.java
@@ -0,0 +1,13 @@
+package com.truman.show.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMain {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaMain.class, args);
+  }
+
+}
