04c18db8e0986df8b47d59d0ce19fd61ecb2b8a3
truman-show <ljh86091@gmail.com>
2020-11-21 23:08:05 +0900

    스프링 테스트 컨텍스트에 대한 학습 테스트

---
+++ toby-study/src/test/java/springbook/learningtest/junit/JUnit.java
@@ -0,0 +1,10 @@
+package springbook.learningtest.junit;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JUnit {
+
+
+
+}
--- toby-study/src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ toby-study/src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -6,16 +6,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = JUnit.class)
 public class JUnitTest {
 
+  @Autowired
+  ApplicationContext context; // 컨텍스트가 매번 주입해주는 애플리케이션 컨텍스트는 항상 같은 오브젝트인지 테스트로 확인해본다.
+
   static Set<JUnitTest> testObject = new HashSet<>();
+  static ApplicationContext contextObject = null;
 
   @Test
   public void test1() {
     System.out.println(testObject);
     assertThat(this).doesNotHaveSameClassAs(testObject);
     testObject.add(this);
+
+    assertThat(contextObject == null || contextObject == this.context).isTrue();
+    contextObject = this.context;
   }
 
   @Test
@@ -23,6 +37,8 @@ public class JUnitTest {
     System.out.println(testObject);
     assertThat(this).doesNotHaveSameClassAs(testObject);
     testObject.add(this);
+    assertThat(contextObject == null || contextObject == this.context);
+    contextObject = this.context;
   }
 
   @Test
@@ -30,5 +46,9 @@ public class JUnitTest {
     System.out.println(testObject);
     assertThat(this).doesNotHaveSameClassAs(testObject);
     testObject.add(this);
+
+    assertThat(contextObject == null || contextObject == this.context);
+    contextObject = this.context;
+
   }
 }
