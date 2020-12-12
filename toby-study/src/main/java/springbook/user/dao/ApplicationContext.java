6e9b581604c22caa5fbccfe27277c505d08223fd
truman-show <ljh86091@gmail.com>
2020-12-12 14:20:36 +0900

    Class 를 이용한 설정파일의 클래스명을 변경하라
    - 클래스 이름을 변경하여 설정파일이라는 점을 명시해줬다
    - DaoFactory, DaoFactoryForTest 이름은 설정파일이름에 적합하다고 생각되지 않았다.

--- toby-study/src/main/java/springbook/user/dao/ApplicationContext.java
+++ toby-study/src/main/java/springbook/user/dao/ApplicationContext.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration  // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
-public class DaoFactory {
+public class ApplicationContext {
 
   @Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시
   public UserDao userDao() {
