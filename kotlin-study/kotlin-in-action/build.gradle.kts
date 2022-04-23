121fa238afbece65750fab35ef0c9cd67c3b5447
ljh86091 <ljh86091@gmail.com>
2022-04-23 11:53:56 +0900

    코틀린 인 액션 학습 모듈을 추가하라

---
+++ kotlin-study/kotlin-in-action/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation(kotlin("stdlib-jdk8"))
+
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
