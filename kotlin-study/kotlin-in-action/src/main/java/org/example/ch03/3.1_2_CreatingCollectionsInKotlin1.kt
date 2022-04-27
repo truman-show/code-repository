83d628ad8ef4b548202ba4bf4c268e326858acf0
ljh86091 <ljh86091@gmail.com>
2022-04-27 09:50:13 +0900

    kotlin-in-action 3장 예제 코드를 추가하라

---
+++ kotlin-study/kotlin-in-action/src/main/java/org/example/ch03/3.1_2_CreatingCollectionsInKotlin1.kt
@@ -0,0 +1,8 @@
+package org.example.ch03
+
+fun main(args: Array<String>) {
+    val strings = listOf("first", "second", "fourteenth")
+    println(strings.last())
+    val numbers = setOf(1, 14, 2)
+//    println(numbers.stream())
+}
