4f2c374c16b9ab8f4cd7295b7fe8914fa29f0f4a
ljh86091 <ljh86091@gmail.com>
2022-05-05 23:18:26 +0900

    예외 엔드포인트를 별도의 컨트롤러에서 관리하도록 분리하라

--- kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/BaseController.kt
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/BaseController.kt
@@ -1,21 +1,14 @@
 package com.example.getinline.controller
 
-import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class BaseController : ErrorController {
+class BaseController {
 
     @GetMapping("/")
     fun root(): String {
-        return "index"
-    }
-
-    @RequestMapping("/error")
-    fun error(): String {
-        return "error"
+        throw Exception("Error Test")
     }
 
 }
