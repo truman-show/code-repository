58d7018dca6879fddadb32a1133399f966d379ce
ljh86091 <ljh86091@gmail.com>
2022-05-13 09:28:12 +0900

    보기좋게 정렬하라

--- kotlin-study/kotlin-in-action/src/main/java/org/example/ch03/3.6_2_LocalFunctionsAndExtensions1.kt
+++ kotlin-study/kotlin-in-action/src/main/java/org/example/ch03/3.6_2_LocalFunctionsAndExtensions1.kt
@@ -1,20 +1,18 @@
-package ch03.ex6_2_LocalFunctionsAndExtensions1
+package org.example.ch03.ex6_2_LocalFunctionsAndExtensions1
 
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
 
-    fun validate(user: User,
-                 value: String,
+    fun validate(value: String,
                  fieldName: String) {
-        if (value.isEmpty()) {
-            throw IllegalArgumentException(
-                "Can't save user ${user.id}: empty $fieldName")
-        }
+        if (value.isEmpty())
+            throw IllegalArgumentException("Can't save user ${user.id}: empty $fieldName")
+
     }
 
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
+    validate(user.name, "Name")
+    validate(user.address, "Address")
 
     // Save user to the database
 }
--- kotlin-study/kotlin-in-action/src/main/java/org/example/ch03/3.6_4_LocalFunctionsAndExtensions3.kt
+++ kotlin-study/kotlin-in-action/src/main/java/org/example/ch03/3.6_4_LocalFunctionsAndExtensions3.kt
@@ -1,12 +1,14 @@
-package ch03.ex6_4_LocalFunctionsAndExtensions3
+package org.example.ch03
 
 class User(val id: Int, val name: String, val address: String)
 
 fun User.validateBeforeSave() {
+
     fun validate(value: String, fieldName: String) {
         if (value.isEmpty()) {
             throw IllegalArgumentException(
-               "Can't save user $id: empty $fieldName")
+                "Can't save user $id: empty $fieldName"
+            )
         }
     }
 
