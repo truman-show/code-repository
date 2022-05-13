f1ce8c33b28819446a5a0fc697a4d79cb1a65809
ljh86091 <ljh86091@gmail.com>
2022-05-13 09:28:27 +0900

    코드를 설명하는 주석을 추가하라

--- kotlin-study/kotlin-in-action/src/main/java/org/example/ch04/4.1.4_1_Button1.kt
+++ kotlin-study/kotlin-in-action/src/main/java/org/example/ch04/4.1.4_1_Button1.kt
@@ -2,7 +2,7 @@ package ch04.Button1
 
 import java.io.Serializable
 
-interface State: Serializable
+interface State : Serializable
 
 interface View {
     fun getCurrentState(): State
@@ -12,7 +12,14 @@ interface View {
 class Button : View {
     override fun getCurrentState(): State = ButtonState()
 
-    override fun restoreState(state: State) { /*...*/ }
+    override fun restoreState(state: State) { /*...*/
+    }
 
+    // 이 클래스는 자바의 정적 중첩 클래스와 대응한다 (class 내부 static class)
     class ButtonState : State { /*...*/ }
+    /*
+    static class ButtonState implements State {
+        /*...*/
+    }
+     */
 }
