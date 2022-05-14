fdde819b476ef78ccd7f0ece4ae7b3dc110eb1b7
ljh86091 <ljh86091@gmail.com>
2022-05-14 09:44:46 +0900

    코드를 설명하는 주석을 추가하라

--- kotlin-study/kotlin-in-action/src/main/java/org/example/ch04/4.2.3_2_ImplementingPropertiesDeclaredInInterfaces1.kt
+++ kotlin-study/kotlin-in-action/src/main/java/org/example/ch04/4.2.3_2_ImplementingPropertiesDeclaredInInterfaces1.kt
@@ -3,5 +3,5 @@ package ch04.ex2_3_2_ImplementingPropertiesDeclaredInInterfaces1
 interface User {
     val email: String
     val nickname: String
-        get() = email.substringBefore('@')
+        get() = email.substringBefore('@')// 프로퍼티에 뒷받침하는 필드가 없다. 대신 매번 결과를 계산해 돌려준다.
 }
