474b51b272d505b26ff0571a79138e74a7410546
ljihoon <ljh86091@gmail.com>
2022-05-28 22:54:42 +0900

    GeneralException 을 잡아 처리하는 ExceptionHandler 를 추가하라
    임시 테스트를 위한 컨트롤러 예외 발생 코드는 이후 테스트 코드로 교정해야한다

--- kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/api/APIEventController.kt
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/api/APIEventController.kt
@@ -1,6 +1,10 @@
 package com.example.getinline.controller.api
 
-import org.springframework.web.HttpRequestMethodNotSupportedException
+import com.example.getinline.dto.APIErrorResponse
+import com.example.getinline.dto.APIErrorResponse.Companion.of
+import com.example.getinline.exception.GeneralException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,7 +13,7 @@ class APIEventController {
 
     @GetMapping("/events")
     fun getEvents(): List<String>? {
-        throw HttpRequestMethodNotSupportedException("스프링 예외 테스트")
+        throw GeneralException("테스트 메세지")
 //        return listOf("event1", "event2")
     }
 
@@ -32,4 +36,19 @@ class APIEventController {
     fun removeEvent(@PathVariable eventId: Int?): Boolean? {
         return true
     }
+
+    // APIEventController 의 핸들러 메소드들 중에서 GeneralException 예외가 발생했을 때 예외를 잡아 후처리를 한다
+    @ExceptionHandler
+    fun general(e: GeneralException): ResponseEntity<APIErrorResponse> {
+        val errorCode = e.errorCode
+        val status =
+            if (errorCode.isClientSideError()) HttpStatus.BAD_REQUEST else HttpStatus.INTERNAL_SERVER_ERROR
+
+        return ResponseEntity
+            .status(status)
+            .body(
+                of(false, errorCode, errorCode.message(e))
+            )
+    }
+
 }
