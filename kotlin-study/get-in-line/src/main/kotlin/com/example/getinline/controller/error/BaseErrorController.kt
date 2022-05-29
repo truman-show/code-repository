3164254d5b01d247c97004b4c0b239d4ea8e31c8
ljihoon <ljh86091@gmail.com>
2022-05-29 11:33:34 +0900

    API 예외 발생 핸들러 메서드를 추가하라

--- kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/error/BaseErrorController.kt
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/error/BaseErrorController.kt
@@ -1,8 +1,11 @@
 package com.example.getinline.controller.error
 
 import com.example.getinline.constant.ErrorCode
+import com.example.getinline.dto.APIErrorResponse
 import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
@@ -11,12 +14,12 @@ import javax.servlet.http.HttpServletResponse
 @Controller
 class BaseErrorController : ErrorController {
 
-    @RequestMapping("/error")
-    fun error(response: HttpServletResponse): ModelAndView {
+    @RequestMapping("/error", produces = [MediaType.TEXT_HTML_VALUE])
+    fun errorHtml(response: HttpServletResponse): ModelAndView {
         val status = HttpStatus.valueOf(response.status)
         val errorCode =
             if (status.is4xxClientError) ErrorCode.BAD_REQUEST else ErrorCode.INTERNAL_ERROR
-        
+
         return ModelAndView(
             "error",
             mapOf<String, Any>(
@@ -26,6 +29,17 @@ class BaseErrorController : ErrorController {
             ),
             status
         )
-
     }
+
+    @RequestMapping("/error")
+    fun error(response: HttpServletResponse): ResponseEntity<APIErrorResponse> {
+        val status = HttpStatus.valueOf(response.status)
+        val errorCode =
+            if (status.is4xxClientError) ErrorCode.BAD_REQUEST else ErrorCode.INTERNAL_ERROR
+
+        return ResponseEntity
+            .status(status)
+            .body(APIErrorResponse.of(false, errorCode))
+    }
+
 }
