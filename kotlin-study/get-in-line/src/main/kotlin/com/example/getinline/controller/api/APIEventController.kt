978d4fafad6b1ad246506ccb4d0b82f0009066cf
ljh86091 <ljh86091@gmail.com>
2022-06-02 00:28:37 +0900

    EventService 의존성을 추가하라

--- kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/api/APIEventController.kt
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/api/APIEventController.kt
@@ -1,24 +1,42 @@
 package com.example.getinline.controller.api
 
+import com.example.getinline.constant.EventStatus
+import com.example.getinline.controller.api.dto.EventDto
 import com.example.getinline.exception.GeneralException
+import com.example.getinline.response.APIDataResponse
 import com.example.getinline.response.APIErrorResponse
 import com.example.getinline.response.APIErrorResponse.Companion.of
+import com.example.getinline.service.EventService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import kotlin.streams.toList
 
 @RestController
 @RequestMapping("/api")
-class APIEventController {
+class APIEventController(private val eventService: EventService) {
 
     @GetMapping("/events")
-    fun getEvents(): List<String>? {
-//        throw GeneralException("테스트 메세지")
-        throw HttpRequestMethodNotSupportedException(
-            "GET"
-        )
-//        return listOf("event1", "event2")
+    fun getEvents(
+        placeId: Long,
+        eventName: String,
+        eventStatus: EventStatus,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) eventStartDatetime: LocalDateTime,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) eventEndDatetime: LocalDateTime
+    ): APIDataResponse<List<EventDto.EventResponse>> {
+
+        val eventResponses: List<EventDto.EventResponse> = eventService.findEvents(
+            placeId,
+            eventName,
+            eventStatus,
+            eventStartDatetime,
+            eventEndDatetime
+        ).stream().map(EventDto.EventResponse::from).toList()
+
+        return APIDataResponse.of(eventResponses)
+
     }
 
     @PostMapping("/events")
