5ec98e2e3222bfd0d2c0ddf8a0e6e320c0f51e9f
ljh86091 <ljh86091@gmail.com>
2022-06-01 22:27:42 +0900

    Event Service 비즈니스 처리 함수를 추가하라

--- kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/service/EventService.kt
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/service/EventService.kt
@@ -2,11 +2,12 @@ package com.example.getinline.service
 
 import com.example.getinline.constant.EventStatus
 import com.example.getinline.controller.api.dto.EventDto
+import com.example.getinline.repository.EventRepository
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
-class EventService {
+class EventService(private val eventRepository: EventRepository) {
 
     fun findEvents(
         placeId: Long?,
@@ -15,7 +16,29 @@ class EventService {
         eventStartDatetime: LocalDateTime?,
         eventEndDatetime: LocalDateTime?
     ): List<EventDto> {
-        return listOf()
+        return eventRepository.findEvents(
+            placeId,
+            eventName,
+            eventStatus,
+            eventStartDatetime,
+            eventEndDatetime
+        )
     }
 
+
+    fun getEvent(eventId: Long?): EventDto? {
+        return eventRepository.findEvent(eventId)
+    }
+
+    fun createEvent(eventDTO: EventDto): Boolean {
+        return eventRepository.insertEvent(eventDTO)
+    }
+
+    fun modifyEvent(eventId: Long?, dto: EventDto): Boolean {
+        return eventRepository.updateEvent(eventId, dto)
+    }
+
+    fun removeEvent(eventId: Long): Boolean {
+        return eventRepository.deleteEvent(eventId)
+    }
 }
