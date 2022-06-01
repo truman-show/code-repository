8651c80a74af59127f6b1d849b279bfd2f77b50a
ljh86091 <ljh86091@gmail.com>
2022-06-01 12:16:41 +0900

    Place API 의 요청 및 응답 을 처리할 DTO 클래스를 추가하라

---
+++ kotlin-study/get-in-line/src/main/kotlin/com/example/getinline/controller/api/dto/PlaceDto.kt
@@ -0,0 +1,29 @@
+package com.example.getinline.controller.api.dto
+
+import com.example.getinline.constant.PlaceType
+
+class PlaceDto {
+
+    data class PlaceResponseDto(
+        val id: Long,
+        val placeType: PlaceType,
+        val placeName: String,
+        val address: String,
+        val phoneNumber: String,
+        val capacity: Int,
+        val memo: String,
+    ) {
+
+        companion object {
+            fun of(
+                id: Long, placeType: PlaceType, placeName: String, address: String,
+                phoneNumber: String, capacity: Int, memo: String,
+            ): PlaceResponseDto {
+                return PlaceResponseDto(
+                    id, placeType, placeName, address, phoneNumber, capacity, memo
+                )
+            }
+        }
+    }
+
+}
