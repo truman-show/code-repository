326597d039ab239c7a9eb3143bd88f657f745015
ljh86091 <ljh86091@gmail.com>
2023-02-09 09:35:13 +0900

    spring websocket 예제를 추가하라

---
+++ spring/springboot-websocket-example/src/main/kotlin/com/example/springbootwebsocketexample/SpringbootWebsocketExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootwebsocketexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootWebsocketExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootWebsocketExampleApplication>(*args)
+}
---
+++ spring/springboot-websocket-example/src/test/kotlin/com/example/springbootwebsocketexample/SpringbootWebsocketExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootwebsocketexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootWebsocketExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
